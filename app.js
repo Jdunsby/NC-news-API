5fe5736b910ac4b17d89dc20a383126617ff1385
Johnny Dunsby <jmdunsby@gmail.com>
2018-06-29 17:46:53 +0100

    add cors middleware to app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 
 module.exports = app;
