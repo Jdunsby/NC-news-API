846abc098b558d1fcba939406384dc081ba4c98d
Johnny Dunsby <jmdunsby@gmail.com>
2018-07-01 18:57:57 +0100

    Implement and test POST /api/topics/:topic_id/articles endpoint

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,6 @@
-const { notFound } = require('boom');
-const { Article, Comment } = require('../models');
+const mongoose = require('mongoose');
+const { notFound, badRequest } = require('boom');
+const { Article, Topic, User } = require('../models');
 const { addCommentCount } = require('../utils/api');
 
 
@@ -8,7 +9,7 @@ const getArticles = (req, res, next) => {
     .populate('belongs_to')
     .populate('created_by')
     .lean()
-    .then(articles => addCommentCount(articles, Comment))
+    .then(articles => addCommentCount(articles))
     .then(articles => res.status(200).send({ articles }))
     .catch(next);
 };
@@ -22,7 +23,7 @@ const getArticleById = (req, res, next) => {
     .lean()
     .then(article => {
       if(!article) throw notFound('Article not found');
-      return addCommentCount(article, Comment);
+      return addCommentCount(article);
     })
     .then(article => res.status(200).send({ article }))
     .catch(next);
@@ -36,15 +37,42 @@ const getArticlesByTopicId = (req, res, next) => {
     .lean()
     .then(articles => {
       if(!articles || !articles.length) throw notFound(`Articles not found for topic: ${topic_id}`);
-      return addCommentCount(articles, Comment);
+      return addCommentCount(articles);
     })
     .then(articles => res.status(200).send({ articles }))
     .catch(next);
 };
 
-// const postArticle = (req, res, next) => {
+const postArticle = (req, res, next) => {
+  const { topic_id } = req.params;
+  const { title, body, created_by } = req.body;
+  if(!mongoose.Types.ObjectId.isValid(created_by)) {
+    throw badRequest(`"created_by" value '${created_by}' is an invalid user ID`);
+  }
 
-// };
+  Promise.all([
+    Topic.findById(topic_id),
+    User.findById(created_by)
+  ])
+    .then(([topic, user]) => {
+      if(!topic) throw notFound(`Topic with ID ${topic_id} does not exist`);
+      if(!user) throw notFound(`User with ID ${created_by} does not exist`);
+      const newArticle = new Article({
+        title,
+        body,
+        created_by,
+        belongs_to: topic_id
+      });
+      return newArticle.save();
+    })
+    .then(article => Article.populate(article, 'belongs_to'))
+    .then(article => Article.populate(article, 'created_by'))
+    .then(article => {
+      article = {...article.toObject(), comment_count: 0};
+      res.status(201).send({ article });
+    })
+    .catch(next);
+};
 
 // const voteOnArticle = (req, res, next) => {
 
@@ -58,7 +86,7 @@ module.exports = {
   getArticles,
   getArticleById,
   getArticlesByTopicId,
-  // postArticle,
+  postArticle,
   // voteOnArticle,
   // deleteArticle
 };
--- spec/api.spec.js
+++ spec/api.spec.js
@@ -19,7 +19,7 @@ describe('API', () => {
 
   after(() => mongoose.disconnect());
 
-  describe('Topics', () => {
+  describe('TOPICS', () => {
     describe('GET /api/topics', () => {
       it('responds with all topics', () => {
         return request
@@ -79,7 +79,7 @@ describe('API', () => {
     });
   });
 
-  describe('Users', () => {
+  describe('USERS', () => {
     describe('GET /api/users ', () => {
       it('responds with all users', () => {
         return request
@@ -141,7 +141,7 @@ describe('API', () => {
     });
   });
 
-  describe('Articles', () => {
+  describe('ARTICLES', () => {
     describe('GET /api/articles', () => {
       it('responds with all articles', () => {
         return request
@@ -268,6 +268,142 @@ describe('API', () => {
             expect(body.statusCode).to.equal(404);
             expect(body.error).to.equal('Not Found');
             expect(body.message).to.equal('Articles not found for topic: 507f191e810c19729de860ea');
+          });
+      });
+    });
+
+    describe('POST /api/topics/:topic_id/articles', () => {
+      it('responds with the successfully posted article', () => {
+        const newArticle = {
+          title: 'The first of many API posts',
+          body: 'Don`t forget to handle your errors!',
+          created_by: `${userDocs[0]._id}`
+        };
+        return request
+          .post(`/api/topics/${topicDocs[0]._id}/articles`)
+          .send(newArticle)
+          .expect(201)
+          .then(({ body }) => {
+            expect(body).to.have.key('article');
+            expect(body.article).to.be.an('object');
+            expect(body.article).to.include.keys('_id', 'title', 'body', 'votes', 'created_at', 'belongs_to', 'created_by', 'comment_count');
+            expect(body.article.title).to.equal(newArticle.title);
+            expect(body.article.body).to.equal(newArticle.body);
+            expect(body.article.votes).to.equal(0);
+            expect(body.article.belongs_to).to.be.an('object');
+            expect(body.article.belongs_to._id).to.equal(`${topicDocs[0]._id}`);
+            expect(body.article.belongs_to.slug).to.equal(topicDocs[0].slug);
+            expect(body.article.created_by).to.be.an('object');
+            expect(body.article.created_by._id).to.equal(`${userDocs[0]._id}`);
+            expect(body.article.created_by.name).to.equal(userDocs[0].name);
+            expect(body.article.created_by.username).to.equal(userDocs[0].username);
+            expect(body.article.created_by.avatar_url).to.equal(userDocs[0].avatar_url);
+            expect(body.article.comment_count).to.equal(0);
+          });
+      });
+
+      it('Error: responds with a 400 error when request contains an invalid topic_id', () => {
+        const newArticle = {
+          title: 'The second of many API posts',
+          body: 'Don`t forget to handle your errors!',
+          created_by: `${userDocs[0]._id}`
+        };
+        return request
+          .post('/api/topics/beetle/articles')
+          .send(newArticle)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body).to.have.all.keys('statusCode', 'error', 'message');
+            expect(body.statusCode).to.equal(400);
+            expect(body.error).to.equal('CastError');
+            expect(body.message).to.equal('Cast to ObjectId failed for value "beetle" at path "_id" for model "topics"');
+          });
+      });
+
+      it('Error: responds with a 404 error when passed a valid topic_id that doesn`t exist', () => {
+        const newArticle = {
+          title: 'The third of many API posts',
+          body: 'Don`t forget to handle your errors!',
+          created_by: `${userDocs[0]._id}`
+        };
+        return request
+          .post('/api/topics/507f191e810c19729de860ea/articles')
+          .send(newArticle)
+          .expect(404)
+          .then(({ body }) => {
+            expect(body).to.have.all.keys('statusCode', 'error', 'message');
+            expect(body.statusCode).to.equal(404);
+            expect(body.error).to.equal('Not Found');
+            expect(body.message).to.equal('Topic with ID 507f191e810c19729de860ea does not exist');
+          });
+      });
+
+      it('Error: responds with a 400 error when request body has no title property', () => {
+        const newArticle = {
+          body: 'Don`t forget to handle your errors!',
+          created_by: `${userDocs[0]._id}`
+        };
+        return request
+          .post(`/api/topics/${topicDocs[0]._id}/articles`)
+          .send(newArticle)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body).to.have.all.keys('statusCode', 'error', 'message');
+            expect(body.statusCode).to.equal(400);
+            expect(body.error).to.equal('ValidationError');
+            expect(body.message).to.equal('articles validation failed: title: Path `title` is required.');
+          });
+      });
+      
+      it('Error: responds with a 400 error when request body has no body property', () => {
+        const newArticle = {
+          title: 'The third of many API posts',
+          created_by: `${userDocs[0]._id}`
+        };
+        return request
+          .post(`/api/topics/${topicDocs[0]._id}/articles`)
+          .send(newArticle)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body).to.have.all.keys('statusCode', 'error', 'message');
+            expect(body.statusCode).to.equal(400);
+            expect(body.error).to.equal('ValidationError');
+            expect(body.message).to.equal('articles validation failed: body: Path `body` is required.');
+          });
+      });
+
+      it('Error: responds with a 400 error when request body has no created_by property', () => {
+        const newArticle = {
+          title: 'The third of many API posts',
+          body: 'Don`t forget to handle your errors!'
+        };
+        return request
+          .post(`/api/topics/${topicDocs[0]._id}/articles`)
+          .send(newArticle)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body).to.have.all.keys('statusCode', 'error', 'message');
+            expect(body.statusCode).to.equal(400);
+            expect(body.error).to.equal('Bad Request');
+            expect(body.message).to.equal('"created_by" value \'undefined\' is an invalid user ID');
+          });
+      });
+      
+      it('Error: responds with a 400 error when request body`s created_by property does not refer to an existing user', () => {
+        const newArticle = {
+          title: 'The third of many API posts',
+          body: 'Don`t forget to handle your errors!',
+          created_by: 'Elon Musk'
+        };
+        return request
+          .post(`/api/topics/${topicDocs[0]._id}/articles`)
+          .send(newArticle)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body).to.have.all.keys('statusCode', 'error', 'message');
+            expect(body.statusCode).to.equal(400);
+            expect(body.error).to.equal('Bad Request');
+            expect(body.message).to.equal('"created_by" value \'Elon Musk\' is an invalid user ID');
           });
       });
     });
