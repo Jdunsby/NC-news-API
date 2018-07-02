c00a5ac5a0214e07b65d4080093e7303674cab26
Johnny Dunsby <jmdunsby@gmail.com>
2018-07-03 00:39:13 +0100

    Implement and test GET /api endpoint

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -2,6 +2,7 @@ process.env.NODE_ENV = 'test';
 const app = require('../app');
 const request = require('supertest')(app);
 const { expect } = require('chai');
+const apiRootData = require('../db/api-root-data');
 
 describe('API', () => {
   describe('GET /', () => {
@@ -10,6 +11,15 @@ describe('API', () => {
         .get('/')
         .expect(200)
         .then(({ body }) => expect(body).to.have.key('welcome'));
+    });
+  });
+
+  describe('GET /api', () => {
+    it('responds with an api contents object', () => {
+      return request
+        .get('/api')
+        .expect(200)
+        .then(({ body }) => expect(body).to.eql(apiRootData));
     });
   });
 });
