cac16561696e225f5616f1245f9ba9f98fbb0313
Johnny Dunsby <jmdunsby@gmail.com>
2018-07-05 15:15:00 +0100

    Remove console.log() from 500 error handling block

--- errors/handle-500.js
+++ errors/handle-500.js
@@ -1,7 +1,6 @@
 const { internal } = require('boom');
 
 const handle500 = (err, req, res) => {
-  console.error('>>>>>', err, '<<<<<');
   const defaultError = internal(err.message);
   res.status(500).send({error: defaultError});
 };
