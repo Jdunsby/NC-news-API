40fa5ee6ed278c1c1533b5ac083215423183f441
Johnny Dunsby <jmdunsby@gmail.com>
2018-07-14 00:08:16 +0100

    Implement and test GET /users/:username/articles endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { getUsers, getUserByUsername } = require('../controllers/users');
+const { getArticlesByUser } = require('../controllers/articles');
 const { withErrorHandling } = require('../utils/api');
 
 router.get('/', withErrorHandling(getUsers));
 router.get('/:username', withErrorHandling(getUserByUsername));
+router.get('/:username/articles', withErrorHandling(getArticlesByUser));
 
 module.exports = router;
